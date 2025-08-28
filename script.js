ef03443e0a3b70718408034541815068e829f0a2
Dhaval Birade <69907022+den864@users.noreply.github.com>
2025-08-28 07:13:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+const btn=document.getElementById("enterBtn");
+
+btn.addEventListener("click",()=>{
+	const content=document.getElementById("status");
+	content.textContent="Entered Metaverse";
+})
